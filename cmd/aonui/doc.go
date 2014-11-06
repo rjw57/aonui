8333bb68fd31e575757b95ef9cb668935dba84f6
Rich Wareham <rjw57@cam.ac.uk>
2014-11-06 12:11:18 +0000

    aonui: add documentation

---
+++ cmd/aonui/doc.go
@@ -0,0 +1,21 @@
+/*
+Aonui is a tool for downloading and managing meteorological data.
+
+Usage:
+
+        aonui command [arguments]
+
+The commands are:
+
+    inv         filter and sort a GRIB2's inventory into "Tawhiri order"
+    reorder     re-order a GRIB2 file into "Tawhiri order"
+
+Use "aonui help [command]" for more information about a command.
+
+Additional help topics:
+
+    tawhiri     "Tawhiri order" for data
+
+Use "aonui help [topic]" for more information about that topic.
+*/
+package main
