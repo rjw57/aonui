1ab03c46c1f404aca9910f811c0b7809da2aae6d
Rich Wareham <rjw57@cam.ac.uk>
2014-11-06 10:27:31 +0000

    aonuiinv: move to subcommand of aonui

---
+++ cmd/aonui/inv.go
@@ -0,0 +1,106 @@
+package main
+
+// Dump the inventory from a GRIB2 file using wgrib2
+
+import (
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/rjw57/aonui"
+)
+
+var cmdInv = &Command{
+	UsageLine: "inv [-nosort] [-nofilter] gribfile",
+	Short:     "filter and sort a GRIB2's inventory into \"Tawhiri order\"",
+	Long: `
+Inv dumps and optionally filters and sorts a GRIB2's inventory into the order
+Tawhiri expects.
+
+The first time this command is run on a file it can take a long time to
+generate output as wgrib2 will need to scan through the entire GRIB2 message.
+
+Tawhiri expects data to be ordered into a C-style array with dimensions
+forecast hour, pressure, parameter, latitude and longitude. "C-style" here
+meaning that adjacent records in the file correspond to changes in longitude.
+
+Longitudes are ordered West-to-East and latitudes are ordered South-to-North.
+Parameters are in the order HGT, UGRD, VGRD.  Pressures are in decreasing
+numerical order. (This is so that the first pressure corresponds to the lowest
+geo-potential height.) Forecast hours are in increasing numerical order.
+
+Inv does not directly deal with latitudes or longitudes but will parse the
+inventory from the specified GRIB2 file and output an inventory on standard
+output which corresponds to the Tawhiri ordering for parameters, pressures and
+forecast hours.
+
+By default inv will sort inventory items as described above but this behaviour
+may be disabled via the -nosort flag.
+
+By default inv will remove inventory items not used by Tawhiri. This behaviour
+can be disabled via the -nofilter flag. In this case non-Tawhiri inventory
+items will be sorted after Tawhiri ones.
+
+With -nosort and -nofilter both enabled, inv should generate an inventory
+identical to that produced by "wgrib2 -s".
+`,
+}
+
+// Command-line flags
+var (
+	noSort, noFilter bool
+)
+
+func init() {
+	cmdInv.Run = runInv // break init loop
+	cmdInv.Flag.BoolVar(&noSort, "nosort", false, "Do not sort inventory into \"Tawhiri order\"")
+	cmdInv.Flag.BoolVar(&noFilter, "nofilter", false, "Do not remove non-tawhiri items")
+}
+
+func runInv(cmd *Command, args []string) {
+	// Get file from  line
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "error: exactly one GRIB2 must be specified\n")
+		setExitStatus(1)
+		return
+	}
+
+	// Load and parse inventory
+	gribFn := args[0]
+	inv, err := aonui.Wgrib2Inventory(gribFn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: failed to parse grib2: %v\n", err)
+		setExitStatus(1)
+		return
+	}
+
+	// Parse items
+	tws := aonui.ToTawhiris(inv)
+
+	// Filter invalid records if asked
+	if !noFilter {
+		filteredTws := []*aonui.TawhiriItem{}
+		for _, tw := range tws {
+			if tw.IsValid {
+				filteredTws = append(filteredTws, tw)
+			}
+		}
+		tws = filteredTws
+	}
+
+	// Sort if asked. Note that sorting in this manner is effectively a
+	// Swartzian transform.
+	if !noSort {
+		sort.Sort(aonui.ByTawhiri(tws))
+	}
+
+	// De-parse
+	inv = aonui.FromTawhiris(tws)
+
+	// Print inventory
+	for _, item := range inv {
+		for _, ln := range item.Wgrib2Strings() {
+			fmt.Println(ln)
+		}
+	}
+}
---
+++ cmd/aonui/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+)
+
+// Commands lists the available commands and help topics.
+// The order here is the order in which they are printed by 'aonui help'.
+var commands = []*Command{
+	cmdInv,
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+	log.SetFlags(0)
+
+	args := flag.Args()
+	if len(args) < 1 {
+		usage()
+	}
+
+	if args[0] == "help" {
+		help(args[1:])
+		return
+	}
+
+	for _, cmd := range commands {
+		if cmd.Name() == args[0] && cmd.Run != nil {
+			cmd.Flag.Usage = func() { cmd.Usage() }
+			if cmd.CustomFlags {
+				args = args[1:]
+			} else {
+				cmd.Flag.Parse(args[1:])
+				args = cmd.Flag.Args()
+			}
+			cmd.Run(cmd, args)
+			exit()
+			return
+		}
+	}
+
+	fmt.Fprintf(os.Stderr, "aonui: unknown subcommand %q\nRun 'aonui help' for usage.\n", args[0])
+	setExitStatus(2)
+	exit()
+}
+
+var exitStatus = 0
+var exitMu sync.Mutex
+
+func setExitStatus(n int) {
+	exitMu.Lock()
+	if exitStatus < n {
+		exitStatus = n
+	}
+	exitMu.Unlock()
+}
+
+var atexitFuncs []func()
+
+func atexit(f func()) {
+	atexitFuncs = append(atexitFuncs, f)
+}
+
+func exit() {
+	for _, f := range atexitFuncs {
+		f()
+	}
+	os.Exit(exitStatus)
+}
